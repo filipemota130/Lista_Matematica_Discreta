919eb0f4d1085537c82af7e183619c18a5ec081c
filipemota130 <filipesimoesmotao@gmail.com>
2021-12-15 08:08:47 -0300

    questão: é primo

---
+++ eh_primo.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int Primo(int n, int i)
+{
+    if (i >= n / 2)
+    {
+        return 1;
+    }
+    else
+    {
+        if (n % i == 0)
+        {
+            return 0;
+        }
+        else
+        {
+            i = i + 1;
+            Primo(n, i);
+        }
+    }
+}
+
+int main()
+{
+    int num;
+    int i = 2;
+    scanf("%i", &num);
+    if (num == 1 || num == 0)
+    {
+        printf("não é primo\n");
+        return 0;
+    }
+    int retorno = Primo(num, i);
+    if (retorno == 0)
+    {
+        printf("não é primo\n", retorno);
+    }
+    else
+    {
+        printf("é primo\n", retorno);
+    }
+    return 0;
+}
